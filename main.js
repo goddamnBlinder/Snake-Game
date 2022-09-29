8c88ca64af0a625c9ead3e42f65823526a165c33
James Chinweike <chinweikechukwudi@gmail.com>
2022-09-29 20:41:20 +0100

    commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const gameBoard = document.querySelector("#gameBoard");
+const ctx = gameBoard.getContext("2d");
+const scoreText = document.querySelector("#scoreText");
+const resetBtn = document.querySelector("#reset");
+const game = gameBoard.width;
+const gameHeight = game
+
